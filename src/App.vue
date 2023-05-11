e6f53424d0fc2b7df3cfd0246ffafa7f4b6ba47f
Shammy Wammy <svassell2@brooklynsteamcenter.org>
2023-05-11 14:54:37 -0400

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {ref} from 'vue'
+
+let todos = ref(['A', 'B', 'C'])
+let newTodo = ref('')
+function a() {
+  todos.value.push(input.value)
+  input.value = ''
+}
+
+function deleteTodo(index) {
+  todos.value.splice(index, 1)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>My Todo Application</h1>
+  <ol>
+<li v-for="(todo, index) in todos">
+  <button>💀</button>
+  {{ index }}
+  {{ todo }}
+</li>
+  </ol>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <input v-model="(newTodo)" @keydown.enter="addTodo" >
+  <button @click="newTodo">Add Todo</button>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
 </style>
