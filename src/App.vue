456c2dbe18cc1aab57c4af62eaf0ed1f98adb24f
Shammy Wammy <svassell2@brooklynsteamcenter.org>
2023-05-15 14:54:38 -0400

    Fixed

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,42 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, watch} from 'vue'
 
-let todos = ref(['A', 'B', 'C'])
+let todos = ref(JSON.parse(window.localStorage.getItem('todos')))
 let newTodo = ref('')
-function a() {
-  todos.value.push({
-      text: newTodo.value,
-      complete: false
-    })
-    newTodo.value = ''
+let input = ref('')
 
-  input.value = ''
-}
+function addTodo () {
+  todos.value.push({
+   text:newTodo.value,
+   complete: false
+    })
+    newTodo.value=''
+  }
 
 function deleteTodo(index) {
   todos.value.splice(index, 1)
 }
+
+watch(todos, function(value) {
+window.localStorage.setItem('todos',JSON.stringify(value))
+}, {deep: true})
 </script>
+
+
+
 <template>
   <h1>My Todo Application</h1>
-  <ol>
-<li v-for="(todo, index) in todos">
-  <button @click="delete">💀</button>
-  {{ index }}
-  {{ todo }}
+<div v-for="(todo, index) in todos">
+  <input type="checkbox" v-model="todo.complete">
+  <button @click="deleteTodo">💀</button>
   {{ todo.text }}
-  <input type="checkbox">
-</li>
-  </ol>
-
-  <input v-model="(newTodo)" @keydown.enter="addTodo" >
-  <button @click="newTodo">Add Todo</button>
+</div>
+  <input v-model="newTodo" @keydown.enter="addTodo">
+  <button @click="addTodo">Add Todo</button>
 </template>
+
+
+
 
 <style>
 
