c87468b4030608f6549d9a543b6638c6679a11a5
Shammy Wammy <svassell2@brooklynsteamcenter.org>
2023-05-16 14:54:59 -0400

    Fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import {ref, watch} from 'vue'
 
-let todos = ref(JSON.parse(window.localStorage.getItem('todos')))
+let todos = ref(JSON.parse(window.localStorage.getItem('todos'))??[])
 let newTodo = ref('')
 let input = ref('')
 
@@ -26,9 +26,10 @@ window.localStorage.setItem('todos',JSON.stringify(value))
 
 <template>
   <h1>My Todo Application</h1>
-<div v-for="(todo, index) in todos">
+<div v-for="(todo, index) in todos" :class="{completed: todo.complete}">
   <input type="checkbox" v-model="todo.complete">
   <button @click="deleteTodo">💀</button>
+  {{ index }}
   {{ todo.text }}
 </div>
   <input v-model="newTodo" @keydown.enter="addTodo">
@@ -39,5 +40,24 @@ window.localStorage.setItem('todos',JSON.stringify(value))
 
 
 <style>
+body{
+  background: rgb(131,58,180);
+background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
+}
+body{
+  text-align: center;
+}
+body{
+color: indianred;
+} 
+.complete {
+  text-decoration: line-through;
+  color: #c2c2cc;
+}
+input[type="checkbox"]::before {
+  /* ...existing styles */
 
+  transform-origin: bottom left;
+  clip-path: polygon(14% 44%, 0 65%, 50% 100%, 100% 16%, 80% 0%, 43% 62%);
+}
 </style>
